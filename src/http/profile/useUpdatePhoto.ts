578334b4cf0c1a185b1b69fa0419bd450ebf42c9
Mauricio Macedo da Silva <mauricio.silva@fepese.org.br>
2025-08-13 11:17:53 -0300

    feat: code adjusted

--- src/http/profile/useUpdatePhoto.ts
+++ src/http/profile/useUpdatePhoto.ts
@@ -5,38 +5,44 @@ import { useApi } from "@/hooks/useApi";
 import { useAuth } from "@/hooks/useAuth";
 
 export function UseUpdatePhoto() {
-    const { user, updateUser } = useAuth();
-    const { fetchWithAuth } = useApi();
+  const { user, updateUser } = useAuth();
+  const { fetchWithAuth } = useApi();
 
-    return useMutation({
-        mutationFn: async (data: { file: File }) => {
-            if (!data.file) {
-                throw new Error("arquivo é obrigatorio");
-            }
+  return useMutation({
+    mutationFn: async (data: { file: File }) => {
+      if (!data.file) {
+        throw new Error("arquivo é obrigatorio");
+      }
 
-            const formData = new FormData();
-            formData.append("file", data.file);
+      const formData = new FormData();
+      formData.append("file", data.file);
 
-            const response = await fetchWithAuth(
-                `${environment.apiUrl}/${environment.apiVersion}/profile/${user?.id}/photo`,
-                {
-                    method: "POST",
-                    credentials: "include",
-                    body: formData
-                }
-            );
-
-            const result = await response.json();
-            return result;
-        },
-        onSuccess: (data) => {
-            updateUser(data);
-
-            showAutoDismissAlert({
-                message: "Dados salvos com sucesso!",
-                description: "Os dados foram atualizados.",
-                duration: 2000,
-            });  
+      const response = await fetchWithAuth(
+        `${environment.apiUrl}/${environment.apiVersion}/profile/${user?.id}/photo`,
+        {
+          method: "POST",
+          credentials: "include",
+          body: formData,
         }
-    });
+      );
+
+      const result = await response.json();
+      return result;
+    },
+    onSuccess: async (data) => {
+      const userData = data.data || data;
+
+      if (userData) {
+        updateUser(userData);
+      } else {
+        console.error("Nenhum dado de usuário encontrado na resposta");
+      }
+
+      showAutoDismissAlert({
+        message: "Foto atualizada com sucesso!",
+        description: "A imagem foi atualizada.",
+        duration: 2000,
+      });
+    },
+  });
 }

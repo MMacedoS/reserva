7bc5396fad57f1d50b73049369449cb37897a9ff
Mauricio Macedo da Silva <mauricio.silva@fepese.org.br>
2025-09-06 16:01:28 -0300

    feat: ajustado url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://ap1.encantodasaguasquentes.com.br/",
+  apiUrl: "https://ap1.encantodasaguasquentes.com.br",
   apiVersion: "api/v1",
   appVersion: "1.0.40",
   currentYear: "2025",
